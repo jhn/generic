1532870a9c21507514d5d5ff73784b07cee89146
Samurdha Jayasinghe <sj2564@columbia.edu>
2015-10-24 18:50:47 -0400

    add route for post /config/field

--- service/app.js
+++ service/app.js
@@ -1,14 +1,71 @@
+var resourceName = process.env.GENERIC_RESOURCE_NAME;
+var resourceIDName = process.env.GENERIC_RESOURCE_ID_NAME;
+var resourceIDType = getType(process.env.GENERIC_RESOURCE_ID_TYPE);
+
+function getType(typeString) {
+  switch (typeString) {
+    case "Number": 
+      return Number;
+      break;
+    case "Date": 
+      return Date;
+      break;
+    case "Array":
+      return Array;
+      break;
+    case "Boolean":
+      return Boolean;
+      break;
+    case "Object":
+      return Object;
+      break;
+    default:
+      return String;
+  };
+}
+
+var resourceIDRegex = process.env.GENERIC_RESOURCE_ID_REGEX;
+var servicePort = process.env.GENERIC_SERVICE_PORT;
+
 var express = require('express');
+var bodyParser = require('body-parser');
+
+var mongoose = require('mongoose');
+
+mongoose.connect('mongodb://localhost/service-' + resourceName);
+
+var Schema = mongoose.Schema;
+
+var ResourceSchema = new Schema({
+  id: { type: resourceIDType, required: true },
+  data: { type: Object, required: true }
+});
+
+var Resource = mongoose.model('Resource', ResourceSchema);
+
+
+
+var FieldSchema = new Schema({
+  name: { type: String, required: true, unique: true },
+  type: { type: String, required: true },
+  required: Boolean,
+  validation: String
+});
+
+FieldSchema.add({ subfields: [FieldSchema] });
+
+var Field = mongoose.model('Field', FieldSchema);
+
+var config = require('./config')(mongoose, Field);
+var resource = require('./resource');
 
 var app = express();
 
-var resourceName = process.env.GENERIC_RESOURCE_NAME;
-var resourceIDName = process.env.GENERIC_RESOURCE_ID_NAME;
-var resourceIDType = process.env.GENERIC_RESOURCE_ID_TYPE;
-var servicePort = process.env.GENERIC_SERVICE_PORT;
+app.use(bodyParser.urlencoded());
+app.use(bodyParser.json());
 
-app.use('/config', require('./config'));
-app.use('/' + resourceName, require('./resource'));
+app.use('/config', config.router);
+app.use('/' + resourceName, resource.router);
 
 app.listen(servicePort ? servicePort : 3000, function() {
   console.log('listening on port: ' + (servicePort ? servicePort : 3000));
