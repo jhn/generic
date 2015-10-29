25a70d9101c3e56a4292e3d483f5f405088baa38
Dustin Burge <dustin@gethightower.com>
2015-10-29 15:09:01 -0400

    Finished get, put, delete paths.

--- service/config.js
+++ service/config.js
@@ -1,3 +1,5 @@
+var _ = require('lodash');
+
 module.exports = function(mongoose, Field) {
 
   var configRouter = require('express').Router();
@@ -11,6 +13,38 @@ module.exports = function(mongoose, Field) {
     field.save(function(err, f) {
       if (err) { return res.status(500).json(err); }
       return res.status(200).json(f);
+    });
+  });
+
+  configRouter.put('/field/:name', function(req, res, next) {
+    Field.find({ 'name': req.params.name }, function(err, field) {
+      if (err) { return res.status(404).json(err); }
+      var updated = _.merge(field, req.body);
+      updated.save(function(err, f) {
+        if (err) { return res.status(500).json(err); }
+        return res.status(200).json(f);
+      });
+    });
+  });
+
+  configRouter.get('/field/', function(req, res, next) {
+    Field.find({}, function(err, fields) {
+      if (err) { return res.status(500).json(err); }
+      return res.status(200).json(fields);
+    });
+  });
+
+  configRouter.get('/field/:name', function(req, res, next) {
+    Field.find({ 'name': req.params.name }, function(err, field) {
+      if (err) { return res.status(404).json(err); }
+      return res.status(200).json(field);
+    });
+  });
+
+  configRouter.delete('/field/:name', function(req, res, next) {
+    Field.remove({ 'name': req.params.name }, function(err) {
+      if (err) { return res.status(404).json(err); }
+      return res.status(204).send();
     });
   });
 
