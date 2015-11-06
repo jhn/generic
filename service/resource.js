5882cd8fcaa31cee6e9a22750fb34300643b86a3
Dustin Burge <dustin@gethightower.com>
2015-11-06 12:12:33 -0500

    Added PUT and GET individual resource routes.

--- service/resource.js
+++ service/resource.js
@@ -23,10 +23,30 @@ module.exports = function(mongoose) {
       });
     });
 
+    resourceRouter.put('/:id', function(req, res, next) {
+      require('./validator')(Field, function(validator) {
+        validResource = validator.validate(req.body);
+        if (!validResource) {
+          return res.status(400).send('Message body contains an invalid resource.');
+        }
+        Resource.findOneAndUpdate({ [resourceIDName]: req.params.id }, { $set: req.body }, { upsert: true, new: true }, function(err, resource) {
+          if (err) { return res.status(500).json(err); }
+          return res.status(200).json(resource);
+        });
+      });
+    });
+
     resourceRouter.get('/', function(req, res, next) {
       Resource.find({}, function(err, resources) {
         if (err) { return res.status(500).json(err); }
         return res.status(200).json(resources);
+      });
+    });
+
+    resourceRouter.get('/:id', function(req, res, next) {
+      Resource.find({ [resourceIDName]: req.params.id }, function(err, resource) {
+        if (err) { return res.status(404).json(err); }
+        return res.status(200).json(resource);
       });
     });
 
