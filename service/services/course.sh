9dee231f0394b0672197ad3c72f697e9d4d23f6d
Dustin Burge <dustin@gethightower.com>
2015-11-12 17:17:15 -0500

    Scripts! And a couple bugfixes.

---
+++ service/services/course.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+export GENERIC_SERVICE_PORT=$PORT
+export GENERIC_RESOURCE_NAME=course
+export GENERIC_RESOURCE_ID_NAME=cid
+export GENERIC_RESOURCE_ID_TYPE=String
+export GENERIC_KAFKA_ZK=localhost:2181
+export GENERIC_KAFKA_TOPIC=test
+
+cmd="node app.js"
+
+$cmd &
+
+sleep 3
+
+address="http://localhost:$PORT/config/field"
+echo $address
+curl -X POST -H "Content-type: application/json" --data "{\"name\": \"professor\",\"required\": true,\"type\": \"String\",\"subfields\": []}" $address
+curl -X POST -H "Content-type: application/json" --data "{\"name\": \"course_name\",\"required\": true,\"type\": \"String\",\"subfields\": []}" $address
+curl -X POST -H "Content-type: application/json" --data "{\"name\": \"students\",\"required\": true,\"type\": \"Array\",\"subfields\": []}" $address
