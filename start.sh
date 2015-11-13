b98e286f7e28078fef2d35fbef551561ff62eb73
Johan Mena <johan.menac@gmail.com>
2015-11-12 20:37:32 -0500

    Add more specific routes for services

--- start.sh
+++ start.sh
@@ -1,17 +1,18 @@
  #!/usr/bin/env bash
 
 echo "Registering services"
-curl -X POST http://localhost:3000/register --data '{"name": "students-a-f", "regex": "/student/[a-f]", "out": "160.39.182.146:8001"}' --header "Content-type:application/json"
+curl -X POST http://localhost:3000/register --data '{"name": "students-a-f", "regex": "/student/[a-f][^/]*$", "out": "160.39.182.146:8001"}' --header "Content-type:application/json"
 echo ""
 sleep 1
-curl -X POST http://localhost:3000/register --data '{"name": "students-g-m", "regex": "/student/[g-m]", "out": "160.39.182.146:8002"}' --header "Content-type:application/json"
+curl -X POST http://localhost:3000/register --data '{"name": "students-g-m", "regex": "/student/[g-m][^/]*$", "out": "160.39.182.146:8002"}' --header "Content-type:application/json"
 echo ""
 sleep 1
-curl -X POST http://localhost:3000/register --data '{"name": "students-n-z", "regex": "/student/[n-z]", "out": "160.39.182.146:8003"}' --header "Content-type:application/json"
+curl -X POST http://localhost:3000/register --data '{"name": "students-n-z", "regex": "/student/[n-z][^/]*$", "out": "160.39.182.146:8003"}' --header "Content-type:application/json"
 echo ""
 sleep 1
 curl -X POST http://localhost:3000/register --data '{"name": "courses", "regex": "/course", "out": "160.39.182.146:8004"}' --header "Content-type:application/json"
 echo ""
+sleep 1
 curl -X POST http://localhost:3000/register --data '{"name": "students", "regex": "/student", "out": "160.39.182.146:8001"}' --header "Content-type:application/json"
 sleep 1
 echo ""
