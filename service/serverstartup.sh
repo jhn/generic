bbe34869f746e271e7d7f3fe50e53e4e62b2cd3b
Dustin Burge <dustin@gethightower.com>
2015-11-13 07:22:50 -0500

    Slightly modififed demo script.

--- service/serverstartup.sh
+++ service/serverstartup.sh
@@ -9,7 +9,7 @@ PORT=8004 services/course.sh
 
 sleep 5
 
-address="http://localhost:8001/config/field"
+address="http://localhost:8001/student/config/field"
 curl -X POST -H "Content-type: application/json" --data "{\"name\": \"first\",\"required\": true,\"type\": \"String\",\"subfields\": []}" $address
 curl -X POST -H "Content-type: application/json" --data "{\"name\": \"last\",\"required\": true,\"type\": \"String\",\"subfields\": []}" $address
 curl -X POST -H "Content-type: application/json" --data "{\"name\": \"courses\",\"required\": true,\"type\": \"Array\",\"subfields\": []}" $address
--- service/services/course.sh
+++ service/services/course.sh
@@ -13,8 +13,7 @@ $cmd &
 
 sleep 3
 
-address="http://localhost:$PORT/config/field"
-echo $address
+address="http://localhost:$PORT/course/config/field"
 curl -X POST -H "Content-type: application/json" --data "{\"name\": \"professor\",\"required\": true,\"type\": \"String\",\"subfields\": []}" $address
 curl -X POST -H "Content-type: application/json" --data "{\"name\": \"course_name\",\"required\": true,\"type\": \"String\",\"subfields\": []}" $address
 curl -X POST -H "Content-type: application/json" --data "{\"name\": \"students\",\"required\": true,\"type\": \"Array\",\"subfields\": []}" $address
