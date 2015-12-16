6bb3ccc47b737f49e91284503e5f2fdadee1aee8
emreozen <eo2312@columbia.edu>
2015-12-15 23:08:29 -0500

    sqs updated

--- integrity/integrityApp/sqs_setup.py
+++ integrity/integrityApp/sqs_setup.py
@@ -1,12 +1,14 @@
 #!/usr/local/bin/python
 
+# Emre Ozen 2015
+
 import os
 import sys
 import string
 import traceback
 import boto
 from boto.sqs.connection import SQSConnection
-from boto.sqs.message import Message
+from boto.sqs.message import Message, RawMessage
 from integrityApp import app
 from flask import render_template
 from flask import request, abort, session
@@ -26,25 +28,13 @@ class sqsManager(object):
     self.conn = boto.sqs.connect_to_region(self.region, aws_access_key_id=self.aws_id, aws_secret_access_key=self.aws_key)
     return self.conn
 
-  # def connect(self, region, aws_id, aws_key):
-  #   self.conn = boto.sqs.connect_to_region(region, aws_access_key_id=aws_id, aws_secret_access_key=aws_key)
-  #   return self.conn
-
   def getAllQ(self):
     queues = self.conn.get_all_queues()
     return queues
 
-  # def getAllQ(self, conn):
-  #   queues = conn.get_all_queues()
-  #   return queues
-
   def getQ(self, qName):
     queue = self.conn.get_queue(qName)
     return queue
-
-  # def getQ(self, conn, qName):
-  #   queue = conn.get_queue(qName)
-  #   return queue
 
   def getQCount(self, conn, qName):
     queue = conn.get_queue(qName)
@@ -80,33 +70,38 @@ class sqsManager(object):
       queue.delete_message(mm)
       mm = queue.read(0)
 
-  def WriteToWriteQueue(self, writeQ, action, endpoint, body):
+  def WriteToWriteQueue(self, action, endpoint, body, headers, message_id, output_queue):
 
-    m = Message()
+    writeQ = self.conn.get_queue(str(output_queue))
+
+    m = RawMessage()
     response =''
 
     if (action == 'GET'):
-      print 'request'
-      response = requests.get(endpoint)
-      print 'done'
+      response = requests.get(endpoint, headers=headers)
 
     elif (action == 'PUT'):
-      response = requests.put(endpoint, data = body)
+      response = requests.put(endpoint, data = json.dumps(body), headers=headers)
 
     elif(action == 'POST'):
-      response = requests.post(endpoint, data = body)
+      response = requests.post(endpoint, data = json.dumps(body),  headers=headers)
 
     elif (action == 'DELETE'):
-      response = requests.delete(endpoint)
+      response = requests.delete(endpoint, headers=headers)
 
     print response.text
-    m.set_body(response.text)
+
+    response_final = {
+      'messageId': str(message_id),
+      'response': str(response.text)
+    }
+
+    m.set_body(json.dumps(response_final)) 
     writeQ.write(m)
 
-  def ReadWriteQueue(self, rName, wName):
+  def ReadWriteQueue(self, rName):
 
     readQ = self.conn.get_queue(rName)
-    writeQ = self.conn.get_queue(wName)
 
     while True: 
       rs = readQ.get_messages()
@@ -116,25 +111,21 @@ class sqsManager(object):
         m = rs[0]
         mb = m.get_body()
         json_body = json.loads(mb)
-        print json_body
+
         action = json_body['action']
         endpoint = json_body['endpoint']
         body = json_body['body']
-        print action
-        print body
-        print body
-        self.WriteToWriteQueue(writeQ, action, endpoint, body)
+        headers = json_body['headers']
+        message_id = json_body['message_id']
+        output_queue = json_body['output_queue']
+
+        self.WriteToWriteQueue(action, endpoint, body, headers, message_id, output_queue)
 
         readQ.delete_message(m)
 
 
-
-# @application.route('/awsSQSJS')
-# def awsSQSJS():
-#   return render_template('awsSQSJS.html')
-
 from config import aws_region, aws_id, aws_key
-from config import Read_Queue, Write_Queue
+from config import Read_Queue
 
 @app.route('/awsSQS_setup')
 def awsSQS():
@@ -142,10 +133,8 @@ def awsSQS():
   sm = sqsManager( aws_region, aws_id, aws_key )    
   conn = sm.connect()
 
-  sm.ReadWriteQueue(Read_Queue, Write_Queue)
+  sm.ReadWriteQueue(Read_Queue)
 
   return 'SQS Started'
 
-# @app.route('/test')
-# def test():
-#   return 'test'
+
